b412b3e7c198676a1ab26b3b1285e48f8f4a767d
Will Sentance <william.sentance@gmail.com>
2014-12-31 11:38:52 -0800

    tidied frontend

--- js/main.js
+++ js/main.js
@@ -10,13 +10,9 @@ var app = angular.module('tawnyOwlApp', [
 
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-    // Home
     .when("/", {templateUrl: "partials/home.html"})
     .when("/signup", {templateUrl: "partials/signup.html"})
     .when("/login", {templateUrl: "partials/login.html"})
-    // Pages
-    // .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/foundation", {templateUrl: "partials/foundation-components.html", controller: "SignupCtrl"})
 }]);
   
 app.controller('UsernameCtrl', function($scope, $http) {
@@ -24,18 +20,4 @@ app.controller('UsernameCtrl', function($scope, $http) {
     console.log(data);
     $scope.A = data.username;
   });
-})  
-
-app.controller('SignupCtrl', function (/* $scope, $location, $http */) {
-  console.log("SignupCtrl Controller reporting for duty.");
-
-  // Activates the Carousel
-  // $('.carousel').carousel({
-  //   interval: 5000
-  // });
-
-  // // Activates Tooltips for Social Links
-  // $('.tooltip-social').tooltip({
-  //   selector: "a[data-toggle=tooltip]"
-  // })
-});
+})
