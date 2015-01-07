89a5a235a3e102f53ecec12be013b351b556fec5
Nick Mandel <nrmandel@ucdavis.edu>
2015-01-06 19:04:35 -0800

    creates an error message when field is changed and user name already exists in sign up page

--- client/js/authControllers.js
+++ client/js/authControllers.js
@@ -20,5 +20,39 @@ app.controller('SignupCtrl', function($scope, $http) {
         $scope.errorMsg = data.err;
       })
   }
-  
+  $scope.checkUnique = function() {
+    console.log('the blur has activated');
+    $http.get("/checkUserExists", {
+        params: {username: $scope.username}
+      })
+      .success(function(data) {
+        console.log('checkuserexists data' , data);
+        if (data) {
+          console.log('this user is already in the system');
+          $scope.alreadyExistErrorMsg = "Not a unique user name!";
+        }
+      })
+  }
 })
+
+// app.directive('alreadyExisting', function (){ 
+//   return {
+//     require: 'ngModel',
+//     link: function(scope, elem, attr, ngModel) {
+//       var alreadyExists = false;
+//       $http.get("/checkUserExists", {
+//           params: {username: $scope.username}
+//         })
+//         .success(function(data) {
+//           console.log('checkuserexists data' , data);
+//           alreadyExists = data;
+//           $scope.errorMsg = data.err;
+//         })
+//       ngModel.$parsers.unshift(function (value) {
+//         console.log(value);
+//         ngModel.$setValidity('unique', !alreadyExists);
+//         return value;
+//       });
+//     }
+//   };
+// });
