f71d529ef045ee6747218d463bca23234fc9ce5b
Alex Zai <azai91@gmail.com>
2015-01-14 14:59:16 -0800

    (feat) change apiKey

--- server/auth/db.js
+++ server/auth/db.js
@@ -2,14 +2,16 @@ var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 var uriUtil = require('mongodb-uri');
 var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 
 // mongoose uri string format is different from vanilla mongodb:
 var mongodbUri = 'mongodb://nmandel:backmassage4@ds031271.mongolab.com:31271/icecommusers';
 var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 
 // 30 second connection timeout reccommended by mongolab:
-var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 
 mongoose.connect(mongooseUri, options);
 var db = mongoose.connection;
@@ -17,15 +19,15 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
   console.log('Connected to DB');
 });
-db.hola = function() {
-  console.log('dora the database explorer says hola');
-}
+
 db.userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true},
-  apiKey: {type: String}
+  apiKey: {type: String},
+  url: {type: String}
 });
+
 // Password verification
 db.userSchema.methods.comparePassword = function(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
@@ -33,6 +35,20 @@ db.userSchema.methods.comparePassword = function(candidatePassword, cb) {
     cb(null, isMatch);
   });
 };
+
+db.userSchema.pre('save', function(next) {
+  console.log('3 reached the bcrypt middleware');
+  var user = this;
+  if(!user.isModified('password')) {
+    return next();
+  }
+
+  var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+  user.password = bcrypt.hashSync(user.password, salt);
+  user.apiKey = bcrypt.hashSync(user.username, salt).slice(10).replace(/\./g, '');
+  next();
+});
+
 db.User = mongoose.model('User', db.userSchema);
 
 module.exports = db;
---
+++ test/userSpec.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect;
+var db = require('../server/auth/db');
+var mongoose = require('mongoose');
+var mongodbUri = 'mongodb://nmandel:backmassage4@ds031271.mongolab.com:31271/icecommusers';
+var mongooseUri = mongoose.connect(mongodbUri);
+
+describe('Database Spec', function() {
+  before(function(done) {
+    db.User.remove({username: 'test'}, function() {
+      done();
+    });
+  });
+
+
+  it('should create a user', function(done) {
+    // console.log(db.User.create);
+    db.User.create({username: 'test', email: "email", password: 'test'}, function(err, createdUser) {
+      console.log('createdUser', createdUser);
+      expect(createdUser.apiKey).to.have.length.gt(48);
+      done();
+    });
+  });
+});
